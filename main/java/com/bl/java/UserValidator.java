fa7daac6d504a11eddd180c8f77637ecb9941e71
chandrakalasv <chandrakalasv1502@gmail.com>
2022-06-05 18:37:23 +0530

    added test case to validate user registration last name

--- main/java/com/bl/java/UserValidator.java
+++ main/java/com/bl/java/UserValidator.java
@@ -3,8 +3,11 @@ package com.bl.java;
 import java.util.regex.Pattern;
 
 public class UserValidator {
-    public boolean userValidName(String userFirstName) {
+    public boolean userValidFirstName(String userFirstName) {
         return Pattern.matches("^[A-Z][a-z]{2,}", userFirstName);
+    }
+    public boolean userValidLastName(String userLastName) {
+        return Pattern.matches("^[A-Z][a-z]{2,}", userLastName);
     }
 
 }
--- test/java/UserNameValidatorTest.java
+++ test/java/UserNameValidatorTest.java
@@ -6,7 +6,14 @@ public class UserNameValidatorTest {
     @Test
     public void givenFirstName_WhenProperFormat_ShouldReturnTrue() {
         UserValidator obj = new UserValidator();
-        boolean result = obj.userValidName("Ashaaa");
+        boolean result = obj.userValidFirstName("Ashaaa");
         Assert.assertEquals(true, result);
     }
+    @Test
+    public void givenLastName_WhenProperFormat_ShouldReturnTrue() {
+        UserValidator obj = new UserValidator();
+        boolean result = obj.userValidLastName("Ushaaa");
+        Assert.assertEquals(true, result);
+    }
+
 }
